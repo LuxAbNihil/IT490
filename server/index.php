cc79d21d2723d39dcc2710211044fd78678ff323
lukaszvikas <lzvikas1@gmail.com>
2018-10-01 17:01:04 -0400

    mysql connection successful

--- server/index.php
+++ server/index.php
@@ -1,17 +1,22 @@
+#!/usr/bin/php
 <?php
+
+ $db = Database::getDB();
+
 class Database{
-    private static $dsn = 'mysql:host=sql.njit.edu;dbname=lz329';
-    private static $username = 'lz329';
-    private static $password = 'ULjm0oKSi';
+    private static $dsn = 'mysql:host=192.168.1.118;port=3306;dbname=users';
+    private static $username = 'user';
+    private static $password = '1111';
     private static $db;
- 
-private function ___construct(){}   
+
+// private function ___construct(){}   
     
 public static function getDB(){
+    echo "there";
     if(!isset(self::$db)){
         try {
             self::$db= new PDO(self::$dsn,self::$username,self::$password);
-            //echo "Connected successfully<br>";
+            echo "Connected successfully<br>";
         } catch(PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
             exit();
